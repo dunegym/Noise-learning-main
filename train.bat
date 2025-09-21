2f0243346f3ec0bd5b4bb18086040f875a591d1b
dunegym <18162519579@189.cn>
2025-09-21 21:46:43 +0800

    修改训练脚本

--- train.bat
+++ train.bat
@@ -6,11 +6,12 @@ echo.
 
 REM 从头开始训练
 echo 从头开始训练...
-python Main.py --is_training=True --is_pretrain=False
-
-REM 如果要继续训练预训练模型，可以取消下面一行的注释并使用：
-REM python Main.py --is_training=True --is_pretrain=True --global_step=562800
+python Main.py --is_training=True --is_pretrain=False --is_testing=False --is_predicting=False --is_batch_predicting=False
 
 echo.
+echo 如果要继续训练预训练模型，请使用以下命令：
+echo python Main.py --is_training=True --is_pretrain=True --global_step=562800 --is_testing=False --is_predicting=False --is_batch_predicting=False
+echo.
+
 echo 训练完成！
 pause
--- train.sh
+++ train.sh
@@ -4,9 +4,9 @@
 echo "开始训练模型..."
 
 # 从头开始训练
-python Main.py --is_training=True --is_pretrain=False
+python Main.py --is_training=True --is_pretrain=False --is_testing=False --is_predicting=False --is_batch_predicting=False
 
-# 如果要继续训练预训练模型，可以使用以下命令：
-# python Main.py --is_training=True --is_pretrain=True --global_step=562800
+echo "如果要继续训练预训练模型，请使用以下命令："
+echo "python Main.py --is_training=True --is_pretrain=True --global_step=562800 --is_testing=False --is_predicting=False --is_batch_predicting=False"
 
 echo "训练完成！"
